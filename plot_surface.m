1aeac7261b6d69c42eceb87f65dcf81ef2750b9c
borgesjose <jose.borges90@hotmail.com>
2022-03-23 22:02:19 -0300

    change plt surface 0.2

--- plot_surface.m
+++ plot_surface.m
@@ -7,19 +7,23 @@ Am_min = 1;
 Am_max = 5;
 
 A = zeros(length(x1),length(x2));
-%%
-
-%%
-
+tipo = 'T2';
 %%
 
 for i=1:length(x2), % De 1 até o No. total de medidas da variavel linguistica...
- for j=1:length(x1),
+ 
+   for j=1:length(x1),
+     
+     if tipo == 'T1'
       Am(j)=Inferencia(x1(j),x2(i),L,'LI');
-      %Am(j) = Inferencia_T2(x1(j),x2(i),L,Param,'NLI');  % pertinencia aos conjuntos fuzzy (curvatura)
-      %Ami = Inferencia_T2_SB(x1(i),x2(j),L,Param,'NLI');
-      %Am(j) = Ami*Am_max + Am_min*(1 - Ami);
- end
+     end
+     
+     if tipo =='T2'
+      Am(j) = Inferencia_T2(x1(j),x2(i),L,Param,'NLI');  % pertinencia aos conjuntos fuzzy (curvatura)
+      Am(j) = Ami*Am_max + Am_min*(1 - Ami);
+     end
+     
+   end
  A(i,:) = Am;
 end
 %%
