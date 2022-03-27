cc3063f3b305c74b0295616b9f608b63466f8f05
borgesjose <jose.borges90@hotmail.com>
2022-03-27 20:23:20 -0300

    edited

--- plot_surface.m
+++ plot_surface.m
@@ -1,3 +1,17 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
+% Universidade Federal do Piauí                       %
+% Campus Ministro Petronio Portela                    %
+% Copyright 2022 -José Borges do Carmo Neto-          %
+% @author José Borges do Carmo Neto                   %
+% @email jose.borges90@hotmail.com                    %
+%  -- Level control of a conical tank using Interval  %
+%  Type-2 Fuzzy Logic PID controllers for the Phase   %
+%  and Gain Margins of the System                     % 
+%  nas margens de fase e de ganho                     %
+%  -- Version: 1.0  - 20/02/2022                      %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+%%
+
 L = 2;
 
 x1=-2*L:0.1:2*L;
@@ -8,6 +22,7 @@ Am_max = 5;
 
 A = zeros(length(x1),length(x2));
 tipo = 'T2';
+
 %%
 
 for i=1:length(x2), % De 1 até o No. total de medidas da variavel linguistica...
